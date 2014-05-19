b9c1e911cf5688152a91251b4a62537950329777
Mickey ngo <ravenvn@yahoo.com.vn>
2014-05-19 18:04:36 +0700

    home page prototype

--- client/index.js
+++ client/index.js
@@ -1,2 +1,9 @@
+Template.index.ranks = function() {
+	// return Ranks.find()
+	return [{username:"Herve.Baran", score:100, rank:1}, {username:"Ha.Do", score:99, rank:2}, {username:"Vinh.Ngo", score:98, rank:3}];
+};
 
+Template.index.pridicts = function() {
+	return [{no:1, name:"Herve.Baran", score:"2 - 1"},{no:2, name:"Ha.Do", score:"0 - 2"}, {no:3, name:"Vinh.Ngo", score:"1 - 1"}];
+}
 
